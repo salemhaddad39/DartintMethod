483a8a6443279e816c47b3d91f1f07c9c15d271a
salemhaddad39 <salemhaddad39@gmail.com>
2025-04-20 23:31:11 +0300

    update

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){
+  String myname="salem";
+  int myage=26;
+  double myhighest=171.5;
+  print(myname[0].toUpperCase());
+   print(myname[0].toLowerCase());
+   print(myage);
+   print(myhighest.ceil());
+   print(myhighest.floor());
+   print(myhighest.round());
+   print(myhighest.isNegative);
+   print(myhighest.abs());
+   
+}
